d23e163bbebfcfab08d3549187811f53b9c32d08
Anurag22022 <102757586+Anurag22022@users.noreply.github.com>
2026-04-03 16:16:52 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "Salaar":"https://www.youtube.com/watch?v=Z1QuBXVrUHU",
+    "Kahani":"https://www.youtube.com/watch?v=icaiYMmpG94&list=RDicaiYMmpG94&start_radio=1",
+    "Lehra Do":"https://www.youtube.com/watch?v=diT_XLLJiF8&list=RDicaiYMmpG94&index=3"
+}
